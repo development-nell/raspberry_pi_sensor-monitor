4a26e49d5d4e8454803fafd7f384faac99e62715
Michael Hollenbeck <hollenbeck.ml@gmail.com>
2017-07-15 09:48:34 -0700

    done goofed up logratate a hyuff hyuff hyuff

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 setup(
 	name='rpi.sensors',
-	version='0.0.2',
+	version='0.0.3',
 	description='A sensor monitor for Raspbian',
 	long_description="Monitor an arbitrary number of sensors and services for arbitrary values and launch handler scripts",
 	url='https://github.com/hollenbeck-ml/raspberry_pi_sensor-monitor',
@@ -19,7 +19,8 @@ setup(
 	install_requires=['datetime','requests','xmltodict','importlib'],
 	data_files=[
 		('/etc/sensor-monitor',['etc/monitor.conf']),
-		('/etc/systemd/system',['etc/sensor-monitor.service'])
+		('/etc/systemd/system',['etc/sensor-monitor.service']),
+		('/etc/logrotate.d/',['etc/sensor-monitor-logrotate'])
 	],
 	scripts=['sensor-monitor']
 )
